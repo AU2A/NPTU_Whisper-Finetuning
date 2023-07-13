5698f3f42555d48088c68de8e9f94bd721b77406
hcilab <AU2A>
2023-07-13 11:43:20 +0800

    20230713

---
+++ install.sh
@@ -0,0 +1,3 @@
+pip3 install huggingface_hub datasets transformers torch evaluate soundfile librosa
+pip3 install git+https://github.com/openai/whisper.git
+pip3 install git+https://github.com/bayartsogt-ya/whisper-multiple-hf-datasets
